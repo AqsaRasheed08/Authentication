e9d0e470b27224d7727608160f7469413571a1b1
aqsa.rasheed <aqsa.rasheed@CS-DT-BASIT>
2023-01-05 00:48:08 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Authentication.Models;
+using Microsoft.AspNetCore.Mvc;
+using GoogleAuthentication.Services;
+
+
+namespace Authentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var clientId = "625335937695-epb98brb8er4teiqvh51u2fiahie07ic.apps.googleusercontent.com";
+            var url = "https://localhost:7158/login/GoogleLoginCallback";
+            var response = GoogleAuth.GetAuthUrl(clientId, url);
+            ViewBag.response = response;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GoogleAuthentication.Services;
+
+namespace Authentication.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<ActionResult> GoogleLoginCallback(string code)
+        {
+            var clientId = "625335937695-epb98brb8er4teiqvh51u2fiahie07ic.apps.googleusercontent.com";
+            var url = "https://localhost:7158/login/GoogleLoginCallback";
+            var clientsecret = "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";
+            var token = await GoogleAuth.GetAuthAccessToken(code, clientId, clientsecret, url);
+            var userProfile = await GoogleAuth.GetProfileResponseAsync(token.AccessToken.ToString());
+            ViewBag.show = userProfile;
+            return View();
+        }
+    }
+}
